a824bf3693f3311c6e807f8fc60d1e0e82172b1f
evilripper <gigarimini@gmail.com>
2012-08-07 12:21:16 +0200

    all prj files

---
+++ GoogleOlympicsDoodleDoping/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace GoogleOlympicsDoodleDoping
+{
+    public partial class Form1 : Form
+    {
+        bool _stopThread;
+
+        private doping _workerObject;
+        private Thread _workerThread;
+        
+        public Form1()
+        {
+            _stopThread = false;
+            _workerObject = new doping();
+            _workerThread = new Thread(_workerObject.doWork);
+            InitializeComponent();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (_stopThread)
+            {
+                _workerObject.Stop = true;
+                _workerThread.Join();  
+            }
+            else
+            {
+
+                _workerThread.Start();
+            }
+            _stopThread = !_stopThread;
+        }
+    }
+}
---
+++ GoogleOlympicsDoodleDoping/doping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GoogleOlympicsDoodleDoping
+{
+    class doping
+    {
+        public bool Stop
+        {
+            get { return _stop; }
+            set { _stop = value; }
+        }
+
+        private bool _stop = false;
+
+        public void doWork()
+        {
+            while (!_stop)
+            {
+                SendKeys.SendWait("{LEFT}");
+                System.Threading.Thread.Sleep(50);   
+                SendKeys.SendWait("{RIGHT}");
+            }
+        }
+    }
+}
